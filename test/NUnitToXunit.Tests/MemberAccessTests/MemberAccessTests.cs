0f1fb0d9138b2dd7286afe15ebe38c65b15e7264
Nattapong Nunpan <aixasz@hotmail.com>
2019-10-24 21:08:26 +0700

    Fix not found test path

--- test/NUnitToXunit.Tests/MemberAccessTests/MemberAccessTests.cs
+++ test/NUnitToXunit.Tests/MemberAccessTests/MemberAccessTests.cs
@@ -17,7 +17,6 @@ namespace NUnitToXunit.Tests
         [InlineData(nameof(MemberAccessTests), "ThatAssertionsCollection")]
         [InlineData(nameof(MemberAccessTests), "CompareOperatorAssertion")]
         [InlineData(nameof(MemberAccessTests), "ThatAssertionsWithCompareOperator")]
-        [InlineData(nameof(MemberAccessTests), "ThatAssertionsWithMessageParameter")]
         [InlineData(nameof(MemberAccessTests), "CollectionAssertTests")]
         public void MemberAccessVisitor_FromNUnitAssert_ToXUnitAssertion(string testCategory, string testCase) =>
             SyntaxSnapshot.RunSnapshotTest(testCategory, testCase);
